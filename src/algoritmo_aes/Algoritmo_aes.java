5a10a7157177c5d501a6152cc8df9e4e4aee0c59
Nilton <nduducbnu@gmail.com>
2020-10-30 22:12:10 -0300

    vamo cripto

--- src/algoritmo_aes/Algoritmo_aes.java
+++ src/algoritmo_aes/Algoritmo_aes.java
@@ -65,68 +65,32 @@ public class Algoritmo_aes {
             byte[] fileByteContent = Files.readAllBytes(file.toPath());
             fileBytesSize = fileByteContent.length;
 
-            //aqui pega o número de blocos arrendondando pra mais...
-            blocksNumber = (int) Math.round(((int) fileBytesSize / 16) + 0.5d);
-
-            //verifica se terá de criar um novo bloco de bytes no final
-            if ((fileBytesSize % 16) == 0) {
-                needNewByteBlock = true;
-            }
-
             byte[][] ret = Utils.divideArray(fileByteContent, 16);
             System.out.println(ret);
-            //itera sobre os bytes
-//            for (int i = 0; i < fileByteContent.length; i++) {
-//                //neste momento é o último bloco de bytes
-//                if (blocksNumber == i - 15) {
-//                    System.out.println("chegou no último bloco");
-//                    //ai vê se precisa adicionar um novo bloco ou não
-//                    if (needNewByteBlock) {
-//                        //adiciona um novo bloco com o número de bytes que faltam
-//
-//                    }
-//
-//                    //cria um novo bloco e com 0
-//                }
-//
-//                //se for 16 é pq fechou um bloco
-//                if (byteCounter == 16) {
-//                    byte[] byteBlock = new byte[16];
-//
-//                    //convert lista em array
-//                    for (int j = 0; j < byteCounter; j++) {
-//                        // byteBlock[j] = b;
-//                    }
-//                    Utils.getHex(byteBlock);
-//
-//                    //verificar se chegou no último bloco
-//                    //CriptografiaArquivo criptArquivo = new CriptografiaArquivo();
-//                    //criptArquivo.IniciaCriptografia(textoSimples, roundKeys);
-//                    bytsToCripto = new ArrayList();
-//                    byteCounter = 0;
-//                }
-//            }
+            for (int i = 0; i < ret.length; i++) {
+                String[][] textoSimples = {{"44", "45", "53", "45"},
+                                           {"4e", "56", "4f", "4c"},
+                                           {"56", "49", "4d", "45"},
+                                           {"4e", "54", "4f", "21"}};
+                CriptografiaArquivo criptArquivo = new CriptografiaArquivo();
+                String[][] sCriptografado = criptArquivo.IniciaCriptografia(textoSimples, roundKeys);
+                //CriptografiaArquivo criptArquivo = new CriptografiaArquivo();
+                //criptArquivo.IniciaCriptografia(textoSimples, roundKeys);
+                //}
+            }
+
+            //Le o arquivo, pega os primeiros 16bytes, 
+            //transformar os 16bytes em hexadecimal como exemplo abaixo e 
+            //enviar para o processo de cifragem
+            //Fazer este processo até o final do arquivo. No último bloco lembra
+            //Fazerr de ver o PKCS#5,
+            //que é preencher o bloco até ficar com 16bytes. Se o último bloco fechar 16 certinho, vai
+            //precisar criar um novo bloco todo preenchido pelo PKCS#5.
+            //for(){
 
         } catch (IOException ex) {
             Logger.getLogger(Algoritmo_aes.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //Le o arquivo, pega os primeiros 16bytes, 
-        //transformar os 16bytes em hexadecimal como exemplo abaixo e 
-        //enviar para o processo de cifragem
-        //Fazer este processo até o final do arquivo. No último bloco lembra
-        //Fazerr de ver o PKCS#5,
-        //que é preencher o bloco até ficar com 16bytes. Se o último bloco fechar 16 certinho, vai
-        //precisar criar um novo bloco todo preenchido pelo PKCS#5.
-        //for(){
-        String[][] textoSimples = {{"44", "45", "53", "45"},
-                                   {"4e", "56", "4f", "4c"},
-                                   {"56", "49", "4d", "45"},
-                                   {"4e", "54", "4f", "21"}};
-        CriptografiaArquivo criptArquivo = new CriptografiaArquivo();
-        String[][] sCriptografado = criptArquivo.IniciaCriptografia(textoSimples, roundKeys);
-        //CriptografiaArquivo criptArquivo = new CriptografiaArquivo();
-        //criptArquivo.IniciaCriptografia(textoSimples, roundKeys);
-        //}
     }
 
     public static void logger(RoundKey roundkey, String[] word, String etapa) {
